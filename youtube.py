0189d71f66d264673a7a34ca13dab503a7f4d3e0
awanjila <abemuchikan@gmail.com>
2016-11-01 16:05:48 +0300

    my initial commit

---
+++ youtube.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+import requests
+import json
+'''
+payload = {'key1': 'value1', 'key2': 'value2'}
+r = requests.get('https://api.github.com/repos/octocat/Hello-World', params=payload)
+#print r.text
+#print r.json()
+print r.headers['Content-Type']
+'''
+
+youtube_api='AIzaSyAYVBZd-ZJxVkYp2YvELtNfRY4BK-s6Vdw'
+
+
+mychannel_id="UCmwVv2nqokqZzzyyFe8hEuA"
+
+myYoutube_activity="https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId=UCmwVv2nqokqZzzyyFe8hEuA&key=AIzaSyAYVBZd-ZJxVkYp2YvELtNfRY4BK-s6Vdw&maxResults=70"
