090e3657ef02c95ff58c4ab4b3f516effc830578
awanjila <abemuchikan@gmail.com>
2016-11-03 01:27:56 +0300

    changing my code to use the assigned variables for key and channel id, to avoid hard coding

--- youtube.py
+++ youtube.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 import urllib2
-import requests
 import json
-'''
-payload = {'key1': 'value1', 'key2': 'value2'}
-r = requests.get('https://api.github.com/repos/octocat/Hello-World', params=payload)
-#print r.text
-#print r.json()
-print r.headers['Content-Type']
-'''
+
 
 youtube_api='AIzaSyAYVBZd-ZJxVkYp2YvELtNfRY4BK-s6Vdw'
-
 mychannel_id="UCmwVv2nqokqZzzyyFe8hEuA"
-
-myYoutube_activity="https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCmwVv2nqokqZzzyyFe8hEuA&key=AIzaSyAYVBZd-ZJxVkYp2YvELtNfRY4BK-s6Vdw"
+myYoutube_activity="https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId="+mychannel_id+"&key="+youtube_api+"&maxResults=50"
 json_data=urllib2.urlopen(myYoutube_activity)
 new_data=json.load(json_data)
 
-for objects in new_data['items']:
-	#print "obj is:"+ str(objects)
-	print objects['snippet']['title']
+for obj in new_data['items']:
+
+	print obj['snippet']["title"]
+
+	#print obj['snippet']['publishedAt']
+#print "for progamming music" + " " + str(new_data['items'][0])
+#print new_data['items']
